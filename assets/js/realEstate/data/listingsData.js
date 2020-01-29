caf0e98990421eb421e67fd5c8a5f7d49e51caf0
tarifaljnidi <tarifaljnidi1986@hotmail.com>
2020-01-29 19:52:06 +0100

    Add files via upload

---
+++ assets/js/realEstate/data/listingsData.js
@@ -0,0 +1,102 @@
+var listingsData = [
+  {
+    address: '20-34 grand ave',
+    city: 'Ridgewood',
+    state: 'NY',
+    rooms: 3,
+    price: 220000,
+    floorSpace: 2000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Apartment',
+    image: 'http://media.equityapartments.com/images/c_crop,x_0,y_0,w_1920,h_1080/c_fill,w_1920,h_1080/q_80/4206-28/the-kelvin-apartments-exterior.jpg'
+  },
+  {
+    address: '3 universal st',
+    city: 'Miami',
+    state: 'FL',
+    rooms: 2,
+    price: 24666,
+    floorSpace: 1430,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Condo',
+    image: 'https://www.adinahotels.com/wp-content/uploads/sites/4/2016/07/adina-melbourne-flinders-street-apartment-hotel-one-and-two-bedroom-apartment-2-2013.jpg'
+  },
+  {
+    address: '1 president plaza',
+    city: 'Richmond',
+    state: 'VA',
+    rooms: 1,
+    price: 345355,
+    floorSpace: 2400,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Single Home',
+    image: 'https://c.o0bg.com/rf/image_960w/Boston/2011-2020/2017/01/09/BostonGlobe.com/Lifestyle/Images/doherty_10names04_liv.jpg'
+  },
+  {
+    address: '889 beemore st',
+    city: 'Newark',
+    state: 'NJ',
+    rooms: 0,
+    price: 80000,
+    floorSpace: 1000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Studio',
+    image: 'http://media.equityapartments.com/images/q_50/f_auto/fl_lossy/685-28/oak-park-apartment-homes-kitchen'
+  },
+  {
+    address: '43 hollywood blvd',
+    city: 'Los Angeles',
+    state: 'CA',
+    rooms: 3,
+    price: 603000,
+    floorSpace: 3000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Condo',
+    image: 'http://cdn.freshome.com/wp-content/uploads/2016/02/design-modern-apartment-1-1025x450.jpg'
+  },
+  {
+    address: '2 main st',
+    city: 'Bedstuy',
+    state: 'NY',
+    rooms: 2,
+    price: 220000,
+    floorSpace: 2000,
+    extras: [
+      'elevator',
+      'pool'
+    ],
+    homeType: 'Multi Home',
+    image: 'http://www.theproserve.com/wp-content/uploads/2013/03/apartment-complex-insurance.jpg'
+  },
+  {
+    address: '730 gates ave',
+    city: 'Springfield',
+    state: 'IN',
+    rooms: 1,
+    price: 150000,
+    floorSpace: 2000,
+    extras: [
+      'elevator',
+      'gym'
+    ],
+    homeType: 'Room',
+    image: 'http://www.carolinacouture.com/wood/wp-content/uploads/2014/09/apartment-contemporary-apartment-in-taiwan-by-fertility-design-and-mirror-glass-accent-stylish-and-elegant-apartment-designs-tiny-ass-apartment-design-ideas-apartment-interior.jpg'
+  }
+]
+
+export default listingsData;
